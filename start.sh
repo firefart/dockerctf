57a0e8a4a03c967338d1c7935e44115788572c01
firefart <firefart@gmail.com>
2022-09-17 15:48:56 +0200

    comment

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ export DOCKER_BUILDKIT=1
 
 docker pull ubuntu:latest
 docker build -t dockerctf:latest --build-arg BUILDKIT_INLINE_CACHE=1 .
+# NET-ADMIN is needed for vpn connections to work from inside the container
 docker run -P --cap-add=NET_ADMIN --rm -it -v $(pwd):/volume dockerctf:latest
 
