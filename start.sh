41f827dc17ade4733d28123cbc5beb256eaf40e1
firefart <105281+firefart@users.noreply.github.com>
2023-04-22 23:06:19 +0200

    update

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ export DOCKER_BUILDKIT=1
 
 docker pull ubuntu:rolling
 docker build -t firefart/dockerctf:latest --build-arg BUILDKIT_INLINE_CACHE=1 .
+mkdir -p "$(pwd)/share"
 # NET-ADMIN is needed for vpn connections to work from inside the container
 # also add DISPLAY env var and mount X11 socket to support gui apps inside the container
-docker run -P --cap-add=NET_ADMIN --rm -it -v $(pwd):/volume -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix firefart/dockerctf:latest
+docker run -P --cap-add=NET_ADMIN --rm -it -v "$(pwd)/share:/volume" -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix firefart/dockerctf:latest
